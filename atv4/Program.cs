febdee203f40209b4d08d1f482906f0cbffce7e6
e d u a r d a <86473064+eduardarn@users.noreply.github.com>
2021-12-09 13:26:58 -0300

    Add files via upload

---
+++ atv4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace atv4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nmatric;
+            Double nnota;
+
+            int Matricula;
+            Double nnota;
+
+            string nome;
+            string nnome = string.Empty;
+
+            nnota = 0;
+            nmatric = 0;
+        
+
+            int contador = 1;
+            while (contador <= 15)
+            {
+                Console.WriteLine("");
+                Console.Write("Informe o nome do aluno: ");
+                Nome = (Console.ReadLine());
+                Console.WriteLine("");
+                Console.Write("Informe a matricula do aluno: ");
+                Matricula = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("");
+                Console.Write("Informe a nota do aluno: ");
+                Nota = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("");
+
+                if (nnota < Nota)
+                {
+                    nnome= Nome;
+                    nmatric = Matricula;
+                    nnota = Nota;
+                }
+                contador++;
+            }
+            Console.WriteLine($"O nome do aluno com a nota mais alta é {nnome}, a Matricula do aluno é : {nmatric} e a Nota que ele recebeu foi: {nnota}");
+        }
+    }
+}
